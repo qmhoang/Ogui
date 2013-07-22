6448b79a8424ac1b4c8a17aa1a94e670f9b1a592
delirium <qmhoang@users.noreply.github.com>
2013-07-22 15:11:13 -0500

    copyright header

--- UI/Control.cs
+++ UI/Control.cs
@@ -1,24 +1,22 @@
-//Copyright (c) 2010 Shane Baker
-//
-//Permission is hereby granted, free of charge, to any person obtaining a copy
-//of this software and associated documentation files (the "Software"), to deal
-//in the Software without restriction, including without limitation the rights
-//to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-//copies of the Software, and to permit persons to whom the Software is
-//furnished to do so, subject to the following conditions:
-
-//The above copyright notice and this permission notice shall be included in
-//all copies or substantial portions of the Software.
-
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-//IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-//FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-//AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-//LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-//OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-//THE SOFTWARE.
-//
-
+// Copyright (c) 2010-2013 Shane Baker, Quang-Minh Hoang
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
 using System;
 using DEngine.Core;
 using Ogui.Core;
@@ -55,7 +53,6 @@ namespace Ogui.UI {
 	/// </summary>
 	public abstract class ControlTemplate : WidgetTemplate {
 		#region Constructors
-
 		/// <summary>
 		/// Default constructor initializes properties to their defaults.
 		/// </summary>
@@ -67,7 +64,6 @@ namespace Ogui.UI {
 		#endregion
 
 		#region Properties
-
 		/// <summary>
 		/// The top left position of this control.  Defaults to the origin (0,0)
 		/// </summary>
@@ -86,7 +82,6 @@ namespace Ogui.UI {
 		#endregion
 
 		#region Public Methods
-
 		/// <summary>
 		/// Calculates the Rect (in screen coordinates) of this control.
 		/// </summary>
@@ -98,7 +93,6 @@ namespace Ogui.UI {
 		#endregion
 
 		#region Layout Helpers
-
 		/// <summary>
 		/// Layout helper - positions the control by setting the upper right coordinates.
 		/// </summary>
@@ -225,7 +219,6 @@ namespace Ogui.UI {
 		#endregion
 
 		#region Private
-
 		private void AlignSouth(Rectangle ofRect, int padding) {
 			Point ourCtr = CalculateRect().Center;
 			Point ofCtr = ofRect.Center;
@@ -269,7 +262,6 @@ namespace Ogui.UI {
 		private void AlignNorthWest(Rectangle ofRect, int padding) {
 			SetBottomRight(ofRect.TopLeft.Shift(-(1 + padding), -(1 + padding)));
 		}
-
 		#endregion
 	}
 
